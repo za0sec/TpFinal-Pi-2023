75ce263a3221db714ca7f855b937398c7796dcfd
ozacagnino <ozacagnino@itba.edu.ar>
2023-07-07 21:10:35 -0300

    creacion del primer struct y arreglo de errores

--- bikeSharing.c
+++ bikeSharing.c
@@ -1,8 +1,19 @@
 #include "bikeSharing.h"
 
-typedef struct bikeSharingCDT{
+typedef struct node * TList;
 
+typedef struct node{
+    char * startDate;
+    size_t station1Id; //id de la estación de inicio
+    char * endDate;
+    size_t station2Id; //id de la estacion de fin
+    size_t isMember; //0 si no es miembro, sino 1
+    char * rideableType; //suponiendo que usamos el mismo ADT para las 2 ciudades
+}TNode;
+
+typedef struct bikeSharingCDT{
     TList first;
+
 
 }bikeSharingCDT;
 
--- bikeSharingMON.c
+++ bikeSharingMON.c
@@ -5,36 +5,9 @@
 
 int main( int argc, char * argv[] ){
 
-    if (argc != 2){
+    if (argc != 3){
         fprintf(stderr, "Arguments should be 2");
         exit(1);
-    }
-
-    char c;
-
-    char * str = NULL;
-
-    char * string[] = NULL;
-
-    int i;
-
-    while (argv[0] != EOF){
-
-        for (i=0; argv[0] != '\n'; i++){
-    
-            c = argv[0][i];
-            
-            if (i%CHUNK)
-                str = realloc(str, (i+CHUNK) * sizeof(char));
-
-            str[i] = argv[0][i];
-
-        }
-
-        str = realloc(str, i * sizeof(char));
-
-        
-
     }
 
 }
